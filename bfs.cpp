69be070c314b67ac52106580d1804921a1180717
vishav999 <112277245+vishav999@users.noreply.github.com>
2023-05-11 14:27:58 +0530

    Breadth First Search

---
+++ bfs.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Graph {
+private:
+    int V; // number of vertices
+    vector<vector<int>> adjList; // adjacency list representation of graph
+
+public:
+    Graph(int V) {
+        this->V = V;
+        adjList.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    void BFS(int start) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+
+            cout << v << " ";
+
+            for (int u : adjList[v]) {
+                if (!visited[u]) {
+                    visited[u] = true;
+                    q.push(u);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    // create a graph with 6 vertices
+    Graph g(6);
+
+    // add edges to the graph
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(2, 4);
+    g.addEdge(3, 4);
+    g.addEdge(3, 5);
+    g.addEdge(4, 5);
+
+    // perform BFS starting from vertex 0
+    cout << "BFS traversal starting from vertex 0: ";
+    g.BFS(0);
+
+    return 0;
+}
